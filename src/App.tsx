ea61c8eaa33de84933fba627c228491f12c43958
GianluccaPaiva <gianluccapaiva@gmail.com>
2026-06-04 15:09:33 -0300

    feat: remove unused Navigate import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "./components/provedores/ThemeProvider"
 import { Toaster } from "sonner"
 import { Login } from "./components/Login"
