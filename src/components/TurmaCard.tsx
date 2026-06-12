bee6b97cc5f81604381e068304aa24777aa728fd
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2026-06-12 15:50:52 -0300

    fix: Ajuste de cores de usuario

--- src/components/TurmaCard.tsx
+++ src/components/TurmaCard.tsx
@@ -46,7 +46,7 @@ export function TurmaCard({ materia, professor, banners, fotoProfessor, sala, tu
             classNameAvatar={`rounded-full object-cover border-2 border-background shadow-sm ${compacto ? 'h-10 w-10' : 'h-14 w-14'}`}
             classNameDiv={`flex h-full w-full shrink-0 items-center justify-center rounded-full text-white text-3xl`}
             foto={fotoProfessor}
-            tipo="materia"
+            tipo="usuario"
             palavra={professor}
           />
         </CardAction>
